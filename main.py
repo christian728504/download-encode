d1efd55b18b25535b3993a6cbded88221bb3320b
Christian Ramirez <chrisram0403@proton.me>
2025-04-02 08:58:52 -0400

    handle json decode error

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from src.download_encode import construct_matrix
+from src.download_encode import construct_matrix, get_file_accessions
+import os
 
 def main():
+    if not os.path.exists('accessions.pkl'):
+        get_file_accessions()
     construct_matrix()
 
 if __name__ == "__main__":
