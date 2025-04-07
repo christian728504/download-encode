de13e1874faebcd3f824a5fd5a41b4a36b2b045b
christian728504 <136763906+christian728504@users.noreply.github.com>
2025-04-07 01:45:36 +0000

    fix main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.encode_matrix import EncodeMatrix
 
 def main():
     encode_matrix = EncodeMatrix(path='output', schema_type='files')
-    # encode_matrix.get_size_of_encode()
-    # encode_matrix.to_static_json()
-    # encode_matrix.to_parquet()
+    encode_matrix.get_size_of_encode()
+    encode_matrix.to_static_json()
+    encode_matrix.to_parquet()
     
     # Method only available for schema_type = 'files'
     encode_matrix.filter_files_parquet()
